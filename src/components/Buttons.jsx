f40409f6dde49601e2795f71adffe66069aa4d66
genilson-dev <programationwork@gmail.com>
2025-04-16 20:13:05 -0300

    Add formatoda calculadora com numeros e alguns butoes

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Buttons.css";
+
+export default props => {
+    return (
+        <button className="button">{props.label}
+        </button>
+    )
+}
+
+
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
 import './Calculator.css';
+import Buttons from '../components/Buttons';
 
 export default class Calculator extends Component {
   render() {
     return (
-      <div className='calculator'></div>
+      <div className='calculator'>
+        <Buttons label="AC"/>
+        <Buttons label="/"/>
+        <Buttons label="7"/>
+        <Buttons label="8"/>
+        <Buttons label="9"/>
+        <Buttons label="X"/>
+        <Buttons label="4"/>
+        <Buttons label="5"/>
+        <Buttons label="6"/>
+        <Buttons label="-"/>
+        <Buttons label="1"/>
+        <Buttons label="2"/>
+        <Buttons label="3"/>
+        <Buttons label="+"/>
+        <Buttons label="0"/>
+        <Buttons label="."/>
+        <Buttons label="="/>
+
+       
+      </div>
     )
   }
 }
