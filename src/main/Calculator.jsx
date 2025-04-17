0cbc6253bcaa7e39ed74920c601ce009416c0e88
genilson-dev <programationwork@gmail.com>
2025-04-17 19:34:37 -0300

    Add funcoes de limpar, e adicionar digitos do display

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,7 +3,17 @@ import './Calculator.css';
 import Buttons from '../components/Buttons';
 import Display from '../components/Display';
 
+const inicialState = {
+  displayValue: "0",
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+  result: 0,
+}
+
 export default class Calculator extends Component {
+  state = { ...inicialState }
   constructor(props) {
     super(props);
     this.clearMemory = this.clearMemory.bind(this);
@@ -12,14 +22,28 @@ export default class Calculator extends Component {
   }
 
   clearMemory = () => {
-    console.log('clearMemory')
+    this.setState({ ...inicialState })
     
   }
   setOperation = (operation) => {
     console.log(operation)    
   }
   addDigit = (n) => {
-    console.log(n)    
+    if (n === '.' && this.state.displayValue.includes('.')) {
+      return
+    }    
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({ displayValue, clearDisplay: false });
+    if(n !== "."){
+      const i = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values]; // Clonando o array, ou, peguando o que ja tem e colocando o novo valor
+      values[i] = newValue; // Colocando o novo valor no array
+      this.setState({ values });
+      console.log(values)
+    }
   }
 
 
@@ -28,7 +52,7 @@ export default class Calculator extends Component {
 
     return (
       <div className='calculator'>
-        <Display value={555}/>
+        <Display value={this.state.displayValue}/>
         <Buttons label="AC" click={this.clearMemory} triple/>
         <Buttons label="/" click={this.setOperation} operation/>
         <Buttons label="7" click={this.addDigit}/>
